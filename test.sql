4aa68883876102aca6211335ed35a16fc7d1fb4e
keatonmurray <112250794+keatonmurray@users.noreply.github.com>
2023-05-22 05:05:57 -0400

    Add files via upload

---
+++ test.sql
@@ -0,0 +1,15 @@
+-- SQL commands to create and drop databases 
+
+CREATE DATABASE myDB;
+USE myDB;
+CREATE TABLE myTB (
+			id INT PRIMARY KEY,
+            fname VARCHAR (255),
+            lname VARCHAR (255),
+            city VARCHAR (255),
+            state VARCHAR (255),
+            country VARCHAR (255),
+            nationality VARCHAR (255)
+);
+
+--DROP DATABASE myDB;
