0c95580c2c5a79493572ca2b19c8c6048cc74e45
keatonmurray <112250794+keatonmurray@users.noreply.github.com>
2023-05-22 05:07:53 -0400

    Update test.sql
    Realigned syntax

--- test.sql
+++ test.sql
@@ -3,13 +3,13 @@
 CREATE DATABASE myDB;
 USE myDB;
 CREATE TABLE myTB (
-			id INT PRIMARY KEY,
-            fname VARCHAR (255),
-            lname VARCHAR (255),
-            city VARCHAR (255),
-            state VARCHAR (255),
-            country VARCHAR (255),
-            nationality VARCHAR (255)
+		id INT PRIMARY KEY,
+            	fname VARCHAR (255),
+            	lname VARCHAR (255),
+            	city VARCHAR (255),
+            	state VARCHAR (255),
+            	country VARCHAR (255),
+            	nationality VARCHAR (255)
 );
 
 --DROP DATABASE myDB;
